365667f19c35aef153d850dc115a3106c3999781
502353971 <mruthyunjay.velagapudi@ge.com>
2016-11-15 20:25:29 +0530

    Initial commit

---
+++ src/main/java/com/springexample/app/Appln.java
@@ -0,0 +1,13 @@
+package com.springexample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Appln {
+	public static void main(String[] args) {
+        SpringApplication.run(Appln.class, args);
+    }
+}
